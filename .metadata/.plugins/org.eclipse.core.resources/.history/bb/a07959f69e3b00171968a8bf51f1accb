cd89bcf29d28d622fc5fcf417ac6ba797194b3ad
NeeleshPoojari <neelesh.poojari@gmail.com>
2017-05-18 14:47:37 +0530

    Added dependencies in pom.xml and updated web.xml to get dispatcher servlet and dispatcher-servlet.xml

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/bb/a07959f69e3b00171968a8bf51f1accb
@@ -0,0 +1,5 @@
+package com.niit.controllers;
+
+public class HomeController {
+
+}
---
+++ ecommerce/src/main/java/com/niit/controllers/HomeController.java
@@ -0,0 +1,14 @@
+
+package com.niit.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/homePage")
+	public String getHomePage() {
+		return "home";
+	}
+}
